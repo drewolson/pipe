8ae4dc8d2320f0bb2fbec070a0f33a53d75da85f
Drew Olson <drew@drewolson.org>
2008-12-09 17:27:19 -0500

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,13 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+require './lib/pipe.rb'
+
+Hoe.new('pipe', Pipe::VERSION) do |p|
+  p.developer('Drew Olson', 'olsonas@gmail.com')
+  p.remote_rdoc_dir = ''
+  p.extra_deps << ['hpricot','>= 0.6.164']
+end
+
+# vim: syntax=Ruby
---
+++ lib/pipe.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+require 'rubygems'
+require 'hpricot'
+
+class Pipe
+  VERSION = '0.1.0'
+  HEADER = %q{<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"><channel>}
+  FOOTER = %q{</channel></rss>}
+
+  attr_reader :content
+
+  def self.create(&block)
+    pipe = Pipe.new
+    pipe.instance_eval(&block)
+    HEADER + pipe.content + FOOTER
+  end
+
+  def initialize
+    @content = ""
+  end
+
+  def feed(url,params)
+    doc = Hpricot.XML(open(url))
+
+    @content += (doc/:item).select do |item|
+      params.all? { |field,pattern| (item/field).to_s =~ pattern }
+    end.to_s
+  end
+end
+
